fa0c4f3a875d1b1ed1657b4d0684adf709ef3de9
Code Crash <50129568+CodeCrashStudios@users.noreply.github.com>
2022-12-18 00:43:26 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO.Compression;
+using System.Text.Json;
+
+namespace JSON2LVL
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                string jsonString = File.ReadAllText(args[0]);
+                Console.WriteLine(jsonString);
+                Level? level = JsonSerializer.Deserialize<Level>(jsonString);
+
+                if (level != null)
+                {
+                    Console.WriteLine(level.width);
+
+                    FileStream fs = File.Create(args[0].Substring(0, args[0].Length-5) + ".lvl");
+                    GZipStream gs = new GZipStream(fs, System.IO.Compression.CompressionLevel.Optimal);
+                    BinaryWriter bw = new BinaryWriter(gs);
+
+                    bw.Write((uint)level.width);
+                    bw.Write((uint)level.height);
+
+                    for (int i = 0; i < level.layers.Length; i++)
+                    {
+                        for (int j = 0; j < level.layers[i].data.Length; j++)
+                        {
+                            bw.Write((ushort)(level.layers[i].data[j] + 1));
+                        }
+                    }
+
+                    bw.Close();
+                    gs.Close();
+                    fs.Close();
+                }
+            }
+        }
+    }
+}
